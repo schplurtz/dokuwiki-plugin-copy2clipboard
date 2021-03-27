eeae671557fe39eccade887add274f3a5a51b5fa
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2021-03-27 08:58:20 +0100

    prends en compte les pre avec numéros de ligne

--- script.js
+++ script.js
@@ -17,6 +17,33 @@ jQuery(function() {
         });
     }, 1500);
   };
+  var response=async function(event) {
+    try {
+      let text=''
+      let lis=event.target.parentElement.getElementsByTagName('li');
+      if( lis.length ) {
+        for( let li of lis ) {
+          text += li.textContent + '\n';
+        }
+      }
+      else {
+        text = event.target.parentElement.textContent;
+      }
+      // Why replace ??? geshi adds an NBSP on each empty line. This is an issue
+      // with python, perl... when you want to run copied code, you get a
+      // syntax error "unexpected \xC2 character" or similar... So remove this
+      // crap. And yes it could remove a legitimate NBSP ; chances are low though.
+      text = text.replace(/^\u00A0$/gm, "");
+      await navigator.clipboard.writeText(text);
+      // event.target.textContent = 'Copied to clipboard'
+      console.log( "copié >>>" + text + "<<<" );
+      messageBox('cp2clipok', LANG.plugins.copy2clipboard.copied);
+    } catch (err) {
+      console.error('Failed to copy!', err);
+      messageBox('cp2clipnok', LANG.plugins.copy2clipboard.error);
+    }
+  };
+
   document.querySelectorAll('pre.code,pre.file').forEach(function(elem) {
     elem.classList.add('cp2clip');
     let cp = document.createElement('button');
@@ -24,22 +51,7 @@ jQuery(function() {
     //cp.appendChild(document.createTextNode('Copy to clipboard')) ;
     cp.classList.add('cp2clip');
     elem.appendChild(cp); // pre.appendChild
-    cp.addEventListener('click', async event => {
-      try {
-        // replace ??? geshi adds an NBSP on each empty line. This is an issue
-        // with python, perl... when you want to run copied code, you get a
-        // syntax error "unexpected \xC2 character" or similar... So remove this
-        // crap. And yes it could remove a legitimate NBSP ; chances are low though.
-        const text = event.target.parentElement.textContent.replace(/^\u00A0$/gm, "");
-        await navigator.clipboard.writeText(text);
-        // event.target.textContent = 'Copied to clipboard'
-        // console.log( "copié >>>" + text + "<<<" );
-        messageBox('cp2clipok', LANG.plugins.copy2clipboard.copied);
-      } catch (err) {
-        console.error('Failed to copy!', err);
-        messageBox('cp2clipnok', LANG.plugins.copy2clipboard.error);
-      }
-    })
+    cp.addEventListener('click', response)
   })
 });
 
