1c306bea454bedb652d5f8fe982a4f47641ecea1
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2021-07-26 18:54:16 +0200

    prevent button from scrolling with <pre> content

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 jQuery(function() {
   if(!navigator.clipboard)
     return;
+  // messageBox( 'id', 'text' ); flash a message at top of screen
   var messageBox=function( id, txt ) {
     const body=document.getElementsByTagName('body')[0];
     const msg=document.createElement('div');
@@ -24,12 +25,14 @@ jQuery(function() {
         });
     }, 1500);
   };
+  // true on MS windows. used to set EOL
   var iswin = (navigator.appVersion.indexOf("Win") != -1);
+  // The async function that respond to click event
   var response=async function(event) {
     try {
       let text='';
       // when line numbers are on, geshi uses <li> tag for each line
-      let lis=event.target.parentElement.getElementsByTagName('li');
+      let lis=event.target.previousSibling.getElementsByTagName('li');
       if( lis.length ) {
         for( let li of lis ) {
           text += li.textContent + '\n';
@@ -37,7 +40,7 @@ jQuery(function() {
       }
       // no line numbers, whole text is directely in <pre> tag
       else {
-        text = event.target.parentElement.textContent;
+        text = event.target.previousSibling.textContent;
         text = text.replace( /\r\n/g, '\n' ); // can happen if page files are prepared on win and dropped in doku tree...
       }
       // Why replace \u00A0 ??? geshi adds an NBSP on each empty line. This is an issue
@@ -63,14 +66,20 @@ jQuery(function() {
     }
   };
 
+  // iterate over all <pre> node and create the needed structure.
+  // <pre>...</pre> ==> <div class="cp2clipcont"><pre>...</pre><button /></div>
   document.querySelectorAll('pre.code,pre.file').forEach(function(elem) {
-    elem.classList.add('cp2clip');
-    let cp = document.createElement('button');
-    cp.setAttribute( 'title', LANG.plugins.copy2clipboard.title);
-    //cp.appendChild(document.createTextNode('Copy to clipboard')) ;
-    cp.classList.add('cp2clip');
-    elem.appendChild(cp); // pre.appendChild
-    cp.addEventListener('click', response)
+    // wrap current node in a div. See https://stackoverflow.com/a/46595686/1831273
+    let container=document.createElement('div');
+    container.classList.add('cp2clipcont');
+    elem.parentNode.insertBefore(container, elem);
+    elem.previousElementSibling.appendChild(elem);
+
+    let cpbutton = document.createElement('button');
+    cpbutton.setAttribute( 'title', LANG.plugins.copy2clipboard.title);
+    //cpbutton.classList.add('cp2clip');
+    container.appendChild(cpbutton);
+    cpbutton.addEventListener('click', response)
   })
 });
 
