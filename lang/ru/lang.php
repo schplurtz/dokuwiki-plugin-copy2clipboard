0572b688601a4c2d901b20e659c0d5d32760d9fa
Aleksandr Selivanov <alexgearbox@yandex.ru>
2023-04-16 23:05:10 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['js']['title']           = 'Копировать в буфер обмена';
+$lang['js']['copied']          = 'Текст скопирован';
+$lang['js']['error']           = 'Ошибка при копировании текста';
