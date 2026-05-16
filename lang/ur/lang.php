40c945404318d2d5c90cdda6cc8ed23f59e7b6cc
Atiq Ur Rehman <mathcity@gmail.com>
2026-05-16 15:00:40 +0200

    Translation update (ur)

---
+++ lang/ur/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Atiq Ur Rehman <mathcity@gmail.com>
+ */
+$lang['js']['title']           = 'کلپ بورڈ پر کاپی کریں';
+$lang['js']['copied']          = 'متن کاپی ہو گیا';
+$lang['js']['error']           = 'متن کاپی کرتے وقت خرابی';
